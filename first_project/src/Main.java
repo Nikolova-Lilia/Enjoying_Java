a2ea4f6317c975a0c8c15d5a38298da970fe1b23
Lilia Nikolova <nikolovalilianenkova@gmail.com>
2025-01-23 16:48:56 +0100

    testing more functions and understanding variables and arrays in java

--- first_project/src/Main.java
+++ first_project/src/Main.java
@@ -1,6 +1,47 @@
+import java.awt.*;
+import java.util.Arrays;
+//import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+//        byte age = 31;
+//        long viewsCount = 3_123_456_789L;
+//        float price = 10.99f;
+//        char letter = 'A';
+//        boolean isEligible = false;
 
-        System.out.println("Hello, World!");
+        //Date now = new Date();
+        //System.out.println(now);
+        //System.out.println(viewsCount);
+
+//        byte x = 1;
+//        byte y = x;
+//        x = 2;
+//        System.out.println(y);
+//        Point point1 = new Point(1, 1);
+//        Point point2 = point1;
+//        point1.x = 2;
+//        System.out.println(point2);
+//        String message = "     Hello World" + "!!    ";
+//        System.out.println(message.trim());
+//        System.out.println(message);
+
+        // c:\Windows\...
+//        String message = "c: \tWindows\\...";
+//        System.out.println(message);
+
+        //arrays
+//        int[] numbers = {2, 3, 5, 1, 4};
+//        System.out.println(numbers.length);
+//        Arrays.sort(numbers);
+//        System.out.println(Arrays.toString(numbers));
+
+        //multidim arrays
+//        int[][] numbers = new int[2][3];
+//        numbers[0][0] = 1;
+//        System.out.println(Arrays.deepToString(numbers));
+//        int[][] ar = { {1, 2, 3}, {4, 5, 6}};
+//        System.out.println(Arrays.deepToString(ar));
+        
     }
 }
